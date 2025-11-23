06d6f2dc195b080f37b43b5684f43c255ff60996
Marvin2727 <fischer.marvin95@gmx.de>
2025-11-23 21:01:42 +0100

    Tabellen erzeugt

--- start_GUI_Sidebar.py
+++ start_GUI_Sidebar.py
@@ -1,6 +1,10 @@
 from PySide6.QtWidgets import QApplication
 from Sidebar import MainWindow
 import sys
+from Data_Base_Connection import prepare_database
+
+# Prepare the database before showing GUI
+prepare_database()
 
 app = QApplication(sys.argv)
 
