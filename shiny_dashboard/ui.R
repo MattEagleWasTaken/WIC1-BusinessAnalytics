09a9266813e1d6b4ba7db0880f40a37ef1a5671e
Marvin2727 <fischer.marvin95@gmx.de>
2025-12-02 20:13:38 +0100

    ok

--- shiny_dashboard/ui.R
+++ shiny_dashboard/ui.R
@@ -1,7 +1,32 @@
+library(shiny)
 library(shinydashboard)
 
-dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )
+    )
+  )
 )
